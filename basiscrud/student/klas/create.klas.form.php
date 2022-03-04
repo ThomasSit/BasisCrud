0b6efc76f9f7dbd2b52d31b498a7aff70c884609
ThomasSit <89969304+ThomasSit@users.noreply.github.com>
2022-03-04 10:43:41 +0100

    Add files via upload

---
+++ basiscrud/student/klas/create.klas.form.php
@@ -0,0 +1,14 @@
+<?php include "../include/header.php";?>
+
+
+    <body>
+    <div class="container">
+        <form action="create.klas.php" method="POST">
+
+            <?php include "inputs.klas.php" ?>
+
+        </form>
+    </div>s
+
+    </body>
+<?php include "../include/footer.php";?>
